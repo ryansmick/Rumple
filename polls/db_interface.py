c4ad5b29c6229cf5da0d133adda94f7586fd887a
Ryan Smick <ryanjsmick@gmail.com>
2017-04-14 16:53:56 -0400

    Added submit_vote function and multiple helper functions
    Implemented submit_vote function in db_interface.py to allow for easy
    submission of a vote. Takes into account whether a given user has
    submitted a vote for that question in the past, and in that case will
    simply update his previous vote.

--- polls/db_interface.py
+++ polls/db_interface.py
@@ -17,3 +17,49 @@ class DBInterface:
         for option in options:
             Option.objects.create(question=question, option_text=option)
 
+    # Add a new vote to a question
+    @staticmethod
+    def submit_vote(user_id, question_id, option_number):
+        # Check if the user has already submitted a vote for the given question
+        options = DBInterface.get_sorted_options_for_question(question_id)
+        votes = DBInterface.get_all_votes_for_question(question_id)
+        
+        # Get previous vote if it exists
+        previous_vote = None
+        for vote in votes:
+            if vote.voter_id == user_id:
+                previous_vote = vote
+                break
+
+        # Identify chosen option
+        chosen_option = options[option_number-1]
+
+        # If the user previously voted on the question, update his vote
+        if previous_vote:
+            # Update vote
+            previous_vote.option = chosen_option
+            previous_vote.save()
+        else:
+            # Create a new vote
+            Vote.objects.create(voter_id=user_id, option=chosen_option)
+
+    # Return list containing all votes for a given question
+    @staticmethod
+    def get_all_votes_for_question(question_id):
+        options = DBInterface.get_sorted_options_for_question(question_id)
+
+        votes = []
+        for option in options:
+            votes += DBInterface.get_votes_for_option(option.id)
+
+        return votes
+
+    # Return the options for a given question in order by their id's
+    @staticmethod
+    def get_sorted_options_for_question(question_id):
+        return Question.objects.get(id=question_id).option_set.order_by('id').all()
+
+    # Return a list of votes for a given option
+    @staticmethod
+    def get_votes_for_option(option_id):
+        return Option.objects.get(id=option_id).vote_set.all()
